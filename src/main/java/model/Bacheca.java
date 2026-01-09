af966530b541b6f255244f8e228bff0d2e7b1670
gennischiazzano <149908393+gennischiazzano@users.noreply.github.com>
2026-01-09 13:43:54 +0100

    Update Bacheca.java

--- src/main/java/model/Bacheca.java
+++ src/main/java/model/Bacheca.java
@@ -4,13 +4,74 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bacheca {
-    private Bacheca_Titolo titolo;
+    private int id;
+    private String titolo;
+    private String tipologia;
+    private String utenteUsername;  // cambiato da int utenteId a String utenteUsername
     private String descrizione;
-    private List<Operazione> OperazioneSuBacheca;
+    private List<Operazione> operazioniSuBacheca;
 
-    public Bacheca(Bacheca_Titolo titolo, String descrizione, List<Operazione> OperazioneSuBacheca) {
+    // Costruttore vuoto (necessario per il controller)
+    public Bacheca() {
+        this.operazioniSuBacheca = new ArrayList<>();
+    }
+
+    // Costruttore completo (utile per creare oggetti più velocemente)
+    public Bacheca(int id, String titolo, String tipologia, String descrizione, String utenteUsername) {
+        this.id = id;
         this.titolo = titolo;
+        this.tipologia = tipologia;
         this.descrizione = descrizione;
-        this.OperazioneSuBacheca = new ArrayList<>();
+        this.utenteUsername = utenteUsername;
+        this.operazioniSuBacheca = new ArrayList<>();
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getTipologia() {
+        return tipologia;
+    }
+
+    public void setTipologia(String tipologia) {
+        this.tipologia = tipologia;
+    }
+
+    public String getUtenteUsername() {
+        return utenteUsername;
+    }
+
+    public void setUtenteUsername(String utenteUsername) {
+        this.utenteUsername = utenteUsername;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public List<Operazione> getOperazioniSuBacheca() {
+        return operazioniSuBacheca;
+    }
+
+    public void setOperazioniSuBacheca(List<Operazione> operazioniSuBacheca) {
+        this.operazioniSuBacheca = operazioniSuBacheca;
     }
 }
