aa838dba9c43c505a004dbce6f2715530c284fc2
gennischiazzano <149908393+gennischiazzano@users.noreply.github.com>
2026-01-09 13:44:55 +0100

    Update Modificate.java

--- src/main/java/model/Modificate.java
+++ src/main/java/model/Modificate.java
@@ -1,7 +1,8 @@
 package Model;
 
-public class Modificate extends Operazione{
-    public Modificate(boolean Stato_Op){
+/** Rappresenta un’operazione di modifica di un ToDo. */
+public class Modificate extends Operazione {
+    public Modificate(boolean Stato_Op) {
         super(Stato_Op);
     }
 }
